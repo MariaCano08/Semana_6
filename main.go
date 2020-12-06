99a639680353e7170b596d0641d089cc4ca4bee7
María Cano <maria.cano4435@alumnos.udg.mx>
2020-12-06 14:15:38 -0600

    Listo, Terminado

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"sort"
 	"strings"
+	//"log"
 )
 
 func main() {
 	var s []string
 	var c string
-	var t int
+	var t, i, j int
 	t = 1
 	fileAscendente, err := os.Create("ascendente.txt")
 	if err != nil {
@@ -25,20 +27,43 @@ func main() {
 	defer fileDescendente.Close()
 
 	fmt.Printf("Catpure cada string que desea ordenar")
-	fmt.Printf("Cuando termine de capturar escriba S")
-	for i := 0; i < t; {
+	fmt.Printf("Cuando termine de capturar escriba S: ")
+	for i = 0; i < t; {
 		fmt.Scan(&c)
 		if "s" == strings.ToLower(c) {
 			t = i + 2
 			break
 		}
+		c = c + "\n"
 		s = append(s, c)
 		i = i + 1
 		t = t + 1
 	}
-	//fmt.Println(s)
+
 	sort.Strings(s)
-	fmt.Println(s)
-	//fmt.Printf(s)
+	ss := ""
+	ss2 := ""
+	for j = 0; j < i; {
+		ss += s[j]
+		j = j + 1
+	}
+	j = j - 1
+	for j >= 0 {
+		ss2 += s[j]
+		j = j - 1
+	}
+
+	b := []byte(ss)
+	e := ioutil.WriteFile("ascendente.txt", b, 0644)
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
+	b = []byte(ss2)
+	e = ioutil.WriteFile("descendente.txt", b, 0644)
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
 
 }
